6b203b67b672af4217802d224d15a118439fa305
Tim-Tadj <77534029+Tim-Tadj@users.noreply.github.com>
2021-04-14 18:14:26 +1000

    test

--- test.py
+++ test.py
@@ -1,3 +1,6 @@
-string = "abcde"
-print(string[len(string)-4])
-print(string[-4])
+import queue
+Prio_q = queue.PriorityQueue()
+Prio_q.put((2, "test"))
+Prio_q.put((1, "test2"))
+print(Prio_q.get()[1])
+print(Prio_q.get()[1])
